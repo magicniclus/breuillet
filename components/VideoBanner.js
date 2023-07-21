77b0ed60c000e0e2746a30a3595eec76f5b7c20c
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-07-21 11:46:53 +0200

    Update VideoBanner.js

--- components/VideoBanner.js
+++ components/VideoBanner.js
@@ -3,7 +3,7 @@ import React from "react";
 const VideoBanner = () => {
   return (
     <div className="bg-[url('/images/background/background_house.png')] bg-left bg-contain bg-no-repeat mt-7 w-full mx-auto max-w-7xl flex lg:flex-row flex-col justify-end bg-blue-200 min-h-[200px] lg:items-end items-center">
-      <div className="lg:w-11/12 w-full flex justify-end  p-10">
+      <div className="lg:w-11/12 w-full flex justify-end p-10">
         <img
           src="/images/background/video_image.png"
           alt="cambium"
