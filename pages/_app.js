1470f1e49c4071b924207bde6cdeee7871a462cf
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-07-22 14:39:12 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,7 @@
 import "../styles/globals.css";
-import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <>
-      <Head></Head>
-      <Component {...pageProps} />
-    </>
-  );
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
