ee8cc921cc11dae4b3a717ec12392f7e55914e82
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-07-22 15:39:06 +0200

    Update Banner.js

--- components/Banner.js
+++ components/Banner.js
@@ -10,7 +10,8 @@ const Banner = () => {
           className="object-contain w-14 sm:mt-0 mb-5"
         />
         <h2 className="ml-5 text-xl font-light font-raleway">
-          Investissez dans une construction DURABLE
+          Investissez dans une construction{" "}
+          <span className="font-bold">DURABLE</span>
         </h2>
       </div>
       <div className="flex lg:flex-row flex-col lg:text-start text-center items-center lg:items-start text-white sm:max-w-[20%] mx-w-[60%] sm:mt-0 mt-14">
@@ -20,7 +21,7 @@ const Banner = () => {
           className="object-contain w-14 sm:mt-0 mb-5"
         />
         <h2 className="ml-5 text-xl font-light font-raleway">
-          Optez pour un acteur de PROXIMITÉ
+          Optez pour un acteur de <span className="font-bold">PROXIMITÉ</span>
         </h2>
       </div>
       <div className="flex lg:flex-row flex-col lg:text-start text-center items-center lg:items-start text-white sm:max-w-[25%] mx-w-[60%] sm:mt-0 mt-14">
@@ -30,7 +31,8 @@ const Banner = () => {
           className="object-contain w-14 sm:mt-0 mb-5"
         />
         <h2 className="ml-5 text-xl font-light font-raleway">
-          Appropriez-vous des des espaces conçus pour VIVRE HEUREUX
+          Appropriez-vous des des espaces conçus pour{" "}
+          <span className="font-bold">VIVRE HEUREUX</span>
         </h2>
       </div>
     </section>
