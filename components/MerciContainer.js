e94f9f5e3f4adbb081682e4da6f1310944ca3e0d
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-07-22 14:39:10 +0200

    Create MerciContainer.js

---
+++ components/MerciContainer.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const MerciContainer = () => {
+  return (
+    <section className="min-h-screen w-full p-10">
+      <div className="bg-gray-200">
+        <h1>Merci</h1>
+        <h2>Votre message a bien été envoyé.</h2>
+        <p>Nous vous recontacterons dans les plus brefs délais.</p>
+      </div>
+    </section>
+  );
+};
+
+export default MerciContainer;
