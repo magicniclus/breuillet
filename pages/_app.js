d0881aa8b1d6c3a9e54dab9a74766762d6c7f840
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-07-24 09:35:41 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
 import "../styles/globals.css";
+import TagManager from "react-gtm-module";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  useEffect(() => {
+    const tagManagerArgs = {
+      gtmId: "AW-11272063965",
+    };
+    TagManager.initialize(tagManagerArgs);
+  }, []);
+  return;
+
+  <Component {...pageProps} />;
 }
 
 export default MyApp;
