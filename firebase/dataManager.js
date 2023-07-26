a0eee177c327c558d30a5a3205712a2c57455f25
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-07-26 12:51:00 +0200

    Update dataManager.js

--- firebase/dataManager.js
+++ firebase/dataManager.js
@@ -3,7 +3,7 @@ import { database } from "./firebase.config";
 
 export function writeUserData(userId, nom, prenom, email, telephone, sexe) {
   return new Promise((resolve, reject) => {
-    set(ref(database, "contacts/" + userId), {
+    set(ref(database, "contactsCambuim/" + userId), {
       nom,
       prenom,
       email,
