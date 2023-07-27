0f56fbe5afba0ab4b1e3536ea359eda3319c51d2
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-07-27 12:05:23 +0200

    Update dataManager.js

--- firebase/dataManager.js
+++ firebase/dataManager.js
@@ -1,14 +1,25 @@
 import { getDatabase, ref, set } from "firebase/database";
 import { database } from "./firebase.config";
 
-export function writeUserData(userId, nom, prenom, email, telephone, sexe) {
+export function writeUserData(userId, nom, prenom, email, telephone, civilite) {
+  // Pour obtenir la date et l'heure actuelles au format français
+  const date = new Date();
+  const day = String(date.getDate()).padStart(2, "0");
+  const month = String(date.getMonth() + 1).padStart(2, "0"); // Les mois sont indexés à partir de 0 en JavaScript
+  const year = date.getFullYear();
+  const hour = String(date.getHours()).padStart(2, "0");
+  const minute = String(date.getMinutes()).padStart(2, "0");
+  const second = String(date.getSeconds()).padStart(2, "0");
+
+  const dateEtHeure = `${day}/${month}/${year} ${hour}:${minute}:${second}`;
   return new Promise((resolve, reject) => {
     set(ref(database, "contactsCambuim/" + userId), {
       nom,
       prenom,
       email,
       telephone,
-      sexe,
+      civilite,
+      dateEtHeure,
     })
       .then(() => {
         resolve("success");
